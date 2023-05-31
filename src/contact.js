6f51cbf04c0be716f2d1b4dc52cdce737643c0ab
marley <74526520+saltxd@users.noreply.github.com>
2023-05-31 12:32:02 -0600

    move directories

--- src/contact.js
+++ src/contact.js
@@ -2,5 +2,12 @@ export function createContactTab() {
     const contactDiv = document.createElement("div");
     // Add content and styles to the contactDiv
     // ...
+    const contactStyles = `
+    .contact-content {
+    font-size: 18px;
+    color: #333;
+    }
+    `
     return contactDiv;
   }
+
--- src/menu.js
+++ src/menu.js
@@ -2,5 +2,11 @@ export function createMenuTab() {
     const menuDiv = document.createElement("div");
     // Add content and styles to the menuDiv
     // ...
+    const menuStyles = `
+    .menu-content {
+    font-size: 16px;
+    color: #555;
+    }
+    `;
     return menuDiv;
   }
